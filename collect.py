f29504ac79844b91bd677369891345a390206925
Чуйко Александр Александрович <79326449+Saveliq@users.noreply.github.com>
2026-06-01 19:31:13 +0300

    delete ahs collect

--- collect.py
+++ collect.py
@@ -333,19 +333,19 @@ def process_server(server, output_dir):
         # Сохраняем собранные данные в JSON-файлы
         # with open(os.path.join(temp_dir, "IML_logs.json"), "w", encoding="utf-8") as f:
         #     json.dump(iml_logs, f, indent=2, ensure_ascii=False)
-
-        # Запрашиваем и сохраняем AHS-файл
-        ahs_links_args = ["days=1", "days=7"]
-        ahs_filename = f"{folder_name}.ahs"
-        ahs_file = os.path.join(temp_dir, ahs_filename)
-        ahs_response = None
-        for ahs_link in ahs_links_args:
-            ahs_response = client.get(f"/ahsdata/{folder_name}.ahs?{ahs_link}")
-            if ahs_response:
-                with open(ahs_file, "wb") as f:
-                    f.write(ahs_response.read)
-                client._log_info(f"AHS-файл сохранен: {ahs_filename}")
-                break
+        if False:
+            # Запрашиваем и сохраняем AHS-файл
+            ahs_links_args = ["days=1", "days=7"]
+            ahs_filename = f"{folder_name}.ahs"
+            ahs_file = os.path.join(temp_dir, ahs_filename)
+            ahs_response = None
+            for ahs_link in ahs_links_args:
+                ahs_response = client.get(f"/ahsdata/{folder_name}.ahs?{ahs_link}")
+                if ahs_response:
+                    with open(ahs_file, "wb") as f:
+                        f.write(ahs_response.read)
+                    client._log_info(f"AHS-файл сохранен: {ahs_filename}")
+                    break
 
         # Переименовываем временную папку в окончательную
         os.rename(temp_dir, final_dir)
